c99049b2dd7c6987053f7824091d2a3d62dbeadf
Kirill Volkovich <volkovich@scand.com>
2017-05-11 19:23:43 +0300

    Implement Transition

--- src/client/components/Transition/Transition.DOCUMENTATION.md
+++ src/client/components/Transition/Transition.DOCUMENTATION.md
@@ -12,7 +12,16 @@ Transition is
 ### Code Example
 
 ```
-<Transition />
+<svg
+  version="1.1"
+  width="100%"
+  height="100%"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <Transition
+
+  />
+</svg>
 ```
 
 ### Component Name
--- src/client/components/Transition/Transition.react.js
+++ src/client/components/Transition/Transition.react.js
@@ -1,22 +1,124 @@
 import React, { Component, PropTypes } from 'react';
 import './Transition.less';
 
+const propTypes = {
+  name: PropTypes.string.isRequired,
+  actions: PropTypes.arrayOf(PropTypes.string),
+  lineWidth: PropTypes.number,
+  color: PropTypes.string,
+  highlightColor: PropTypes.string,
+  description: PropTypes.string,
+  snapGridStep: PropTypes.number,
+  x1: PropTypes.number,
+  y1: PropTypes.number,
+  x2: PropTypes.number,
+  y2: PropTypes.number,
+  isHighlighted: PropTypes.bool,
+  isSnap: PropTypes.bool,
+  onClick: PropTypes.func,
+  onDoubleClick: PropTypes.func,
+  onDragStart: PropTypes.func,
+  onDragStop: PropTypes.func,
+  onDrag: PropTypes.func
+};
+const defaultProps = {
+  lineWidth: 2,
+  color: '#000',
+  highlightColor: "#f00",
+  description: '',
+  snapGridStep: 30,
+  x1: 10,
+  y1: 10,
+  x2: 50,
+  y2: 50,
+  isHighlighted: false,
+  isSnap: false,
+  onClick: () => {},
+  onDoubleClick: () => {},
+  onDragStart: () => {},
+  onDragStop: () => {},
+  onDrag: () => {}
+};
+
 export default
 class Transition extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { };
+  handleStart(e, data) {
+    this.props.onDragStart(e, data);
+  }
+
+  handleStop(e, data) {
+    this.props.onDragStop(e, data);
+  }
+
+  handleDrag(e, data) {
+    this.props.onDrag(e, data);
   }
 
   render() {
+    const {
+      name,
+      actions,
+      lineWidth,
+      color,
+      highlightColor,
+      description,
+      snapGridStep,
+      x,
+      y,
+      isHighlighted,
+      isSnap,
+      onClick,
+      onDoubleClick,
+      onDragStart,
+      onDragStop,
+      onDrag
+    } = this.props;
+
     return (
-      <div className="transition">
-      </div>
+      <DraggableCore
+        grid={isSnap ? [snapGridStep, snapGridStep] : null}
+        onStart={this.handleStart.bind(this)}
+        onStop={this.handleStop.bind(this)}
+        onDrag={this.handleDrag.bind(this)}
+        position={{x: 0, y: 0}}
+      >
+        <g>
+          <rect
+            x={x - radius}
+            y={y - radius}
+            width={radius * 2}
+            height={radius * 2}
+            fill="#fff"
+            stroke={isHighlighted ? highlightColor : 'none'}
+            strokeWidth={lineWidth}
+            onClick={onClick}
+            onDoubleClick={onDoubleClick}
+            onDrag={() => console.log('drag')}
+          />
+          <circle
+            cx={x}
+            cy={y}
+            r={radius}
+            fill="none"
+            stroke={color}
+            strokeWidth={lineWidth}
+            >
+          </circle>
+          <text
+            x={x}
+            y={y}
+            fontFamily="monospace"
+            fontSize="16"
+            alignmentBaseline="middle"
+            textAnchor="middle"
+          >
+            {name}
+          </text>
+        </g>
+      </DraggableCore>
     );
   }
 }
 
-Transition.propTypes = {
-};
-Transition.defaultProps = {
-};
+Transition.propTypes = propTypes;
+Transition.defaultProps = defaultProps;
