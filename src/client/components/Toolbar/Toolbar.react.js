3220969bddbc3fe82802c1497fe249503bb2cc18
Kirill Volkovich <volkovich@scand.com>
2017-05-14 13:33:38 +0300

    Add more buttons to toolbar

--- src/client/components/Toolbar/Toolbar.react.js
+++ src/client/components/Toolbar/Toolbar.react.js
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PureComponent, PropTypes } from 'react';
 import './Toolbar.less';
 import TitledButton from 'opuscapita-react-ui-buttons/lib/TitledButton';
 
@@ -13,6 +13,8 @@ import copySVG from '!!raw-loader!opuscapita-ui-svg-icons/lib/content_copy.svg';
 import pasteSVG from '!!raw-loader!opuscapita-ui-svg-icons/lib/content_paste.svg';
 import panSVG from '!!raw-loader!opuscapita-ui-svg-icons/lib/open_with.svg';
 import selectSVG from '!!raw-loader!opuscapita-ui-svg-icons/lib/touch_app.svg';
+import simulateSVG from '!!raw-loader!opuscapita-ui-svg-icons/lib/all_inclusive.svg';
+import helpSVG from '!!raw-loader!opuscapita-ui-svg-icons/lib/live_help.svg';
 
 const propTypes = {
   controls: PropTypes.arrayOf(PropTypes.shape({
@@ -29,6 +31,7 @@ const defaultProps = {
       action: () => {},
       iconSVG: addSVG,
       title: 'New',
+      label: '',
       active: false,
       disabled: false
     },
@@ -36,6 +39,7 @@ const defaultProps = {
       action: () => {},
       iconSVG: openSVG,
       title: 'Open',
+      label: '',
       active: false,
       disabled: false
     },
@@ -43,57 +47,99 @@ const defaultProps = {
       action: () => {},
       iconSVG: saveSVG,
       title: 'Save',
+      label: '',
       active: false,
-      disabled: false
+      disabled: true
     },
     null,
     {
       action: () => {},
       iconSVG: backSVG,
       title: 'Back',
+      label: '',
       active: false,
-      disabled: false
+      disabled: true
     },
     {
       action: () => {},
       iconSVG: forwardSVG,
       title: 'Forward',
+      label: '',
       active: false,
-      disabled: false
+      disabled: true
     },
     {
       action: () => {},
       iconSVG: cutSVG,
       title: 'Cut',
+      label: '',
       active: false,
-      disabled: false
+      disabled: true
     },
     {
       action: () => {},
       iconSVG: copySVG,
       title: 'Copy',
+      label: '',
       active: false,
-      disabled: false
+      disabled: true
     },
     {
       action: () => {},
       iconSVG: pasteSVG,
       title: 'Paste',
+      label: '',
       active: false,
-      disabled: false
+      disabled: true
     },
     null,
     {
       action: () => {},
       iconSVG: selectSVG,
       title: 'Select objects',
-      active: false,
+      label: '',
+      active: true,
       disabled: false
     },
     {
       action: () => {},
       iconSVG: panSVG,
       title: 'Pan',
+      label: '',
+      active: false,
+      disabled: false
+    },
+    null,
+    {
+      action: () => {},
+      iconSVG: addSVG,
+      title: 'Add State',
+      label: 'State',
+      active: false,
+      disabled: false
+    },
+    {
+      action: () => {},
+      iconSVG: addSVG,
+      title: 'Add Transition',
+      label: 'Transition',
+      active: false,
+      disabled: false
+    },
+    {
+      action: () => {},
+      iconSVG: simulateSVG,
+      title: 'Simulate',
+      label: 'Simulate',
+      active: false,
+      disabled: false
+    },
+    null,
+    {
+      action: () => {},
+      iconSVG: helpSVG,
+      title: 'Help',
+      label: '',
       active: false,
       disabled: false
     }
@@ -101,7 +147,7 @@ const defaultProps = {
 };
 
 export default
-class Toolbar extends Component {
+class Toolbar extends PureComponent {
   render() {
     console.log(backSVG);
     const {
@@ -117,8 +163,11 @@ class Toolbar extends Component {
             key={i}
             svg={control.iconSVG}
             title={control.title}
-            disabled={control.active}
-            isActive={control.selected}
+            disabled={control.disabled}
+            color="#333"
+            label={control.label}
+            contentPosition="before"
+            isActive={control.active}
             onClick={control.action}
           />
         ))}
